6528f227e9efd8260cc44ab25dc9d7a1bbf54a49
Роман Хамидулин <romanhamidulin@ya.ru>
2025-07-26 00:27:28 +0500

    codestyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -5,9 +5,15 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film addFilm(Film film);
+
     Film updateFilm(Film film);
+
     List<Film> getAllFilms();
+
     Film getById(int id);
+
     void deleteFilm(int id);
+
 }
