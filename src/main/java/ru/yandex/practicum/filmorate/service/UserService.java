7081159aabfa7e5d2ba2b22bf5d6393e304d8b35
Роман Хамидулин <romanhamidulin@ya.ru>
2025-07-25 23:54:45 +0500

    логика и контроллеры

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,110 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@Slf4j
+public class UserService {
+    private final UserStorage userStorage;
+
+    @Autowired
+    public UserService(UserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
+
+    public List<User> getAllUsers() {
+        log.info("Получен список всех пользователей.");
+        return userStorage.getAllUsers();
+    }
+
+    public User createUser(User user) {
+        validateUser(user);
+        log.info("Пользователь добавлен.");
+        return userStorage.createUser(user);
+    }
+
+    public User updateUser(User user) {
+        //validateUser(user);
+        log.info("Пользователь обновлен.");
+        return userStorage.updateUser(user);
+    }
+
+    public void deleteUser(int id) {
+        log.info("Пользователь удален.");
+        userStorage.deleteUser(id);
+    }
+
+    public void addFriend(int userId, int friendId) {
+        User user = getUserById(userId);
+        User friend = getUserById(friendId);
+
+        user.getFriends().add(friendId);
+        friend.getFriends().add(userId);
+    }
+
+    public void removeFriend(int userId, int friendId) {
+        User user = getUserById(userId);
+        User friend = getUserById(friendId);
+
+        user.getFriends().remove(friendId);
+        friend.getFriends().remove(userId);
+    }
+
+    public List<User> getFriends(int userId) {
+        User user = getUserById(userId);
+        List<User> friends = new ArrayList<>();
+        for (Integer friendId : user.getFriends()) {
+            friends.add(userStorage.getById(friendId));
+        }
+        return friends;
+    }
+
+    public List<User> getCommonFriends(int userId, int otherUserId) {
+        User user = getUserById(userId);
+        User otherUser = getUserById(otherUserId);
+
+        Set<Integer> commonFriendIds = new HashSet<>(user.getFriends());
+        commonFriendIds.retainAll(otherUser.getFriends());
+
+        List<User> commonFriends = new ArrayList<>();
+        for (Integer friendId : commonFriendIds) {
+            commonFriends.add(userStorage.getById(friendId));
+        }
+        return commonFriends;
+    }
+
+    public User getUserById(int id) {
+        User user = userStorage.getById(id);
+        if (user == null) {
+            throw new NotFoundException("Пользователь с id " + id + " не найден");
+        }
+        return user;
+    }
+    public static void validateUser(User user) {
+
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @.");
+        }
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Логин не может быть пустым и содержать пробелы.");
+        }
+        if ((user.getName() == null) || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        if (user.getBirthday() == null || user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем.");
+        }
+    }
+}
